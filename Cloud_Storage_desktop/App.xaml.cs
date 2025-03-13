57d091b7c79ac0759fff65aaf3472b5494367004
Filomilo <fborowiec@wp.pl>
2025-03-13 22:20:58 +0100

    intial

---
+++ Cloud_Storage_desktop/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Cloud_Storage_desktop;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
